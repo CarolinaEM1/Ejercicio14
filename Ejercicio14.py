4a71392277387737422aca1cb878b3a577f521f2
Carolina Martinez <carolinamtz091@gmail.com>
2023-06-06 21:29:33 -0600

    primera version

---
+++ Ejercicio14.py
@@ -0,0 +1,11 @@
+#Tabla de multiplicar
+
+numero = int(input("Digite un numero"))
+
+lista = []
+for i in range(1,11):
+    lista.append(i*numero)
+
+print(f"\nTabla de multiplicar: \n{lista}")
+
+#Carolina EM
